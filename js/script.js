f8becd77a6fe70a2dc822d0f96ef41ef6e9b8cd0
EdwardJFox <edwardjfox@outlook.com>
2017-02-21 12:23:15 +0000

    Began Styling Main
    - Added multiple scss files for different parts of the site
    - Added in the previous colour values, and created the compatability css classes for them in a new helper.scss file
    - Started bringing in assets for the design look

---
+++ js/script.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $('.openMenu').on('click', function(){    $('nav').removeClass('close').addClass('open');  });  $('.closeButton').on('click', function(){    $('nav').removeClass('open').addClass('close');  });
+});
