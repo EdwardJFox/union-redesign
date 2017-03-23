b3ed30f9dbfd21f83cf13ea9d6643f007e4da4f9
EdwardJFox <edwardjfox@outlook.com>
2017-03-23 14:51:59 +0000

    Event Homepage, mobile styling, assets and maintenance
    - Created a homepage which shows potential styling for during an event
    - Updated the mobile styling for navigation and home, to allow for smoother integration on phones
    - Javascript updates which allow the navigation to be clicked outside of to close it
    - Updates to little logo in the header
    - Updated the html to stop the close animation playing everytime someone goes to the page
    - News background graphic, quick SVG made in illustrator

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-  $('.openMenu').on('click', function(){    $('nav').removeClass('close').addClass('open');  });  $('.closeButton').on('click', function(){    $('nav').removeClass('open').addClass('close');  });
+  $('.openMenu').on('click', function(){    $('nav').removeClass('close').addClass('open opening');    setTimeout(function(){      $('nav').removeClass('opening');    }, 800)  });  $('.closeButton').on('click', function(){    $('nav').removeClass('open').addClass('close');  });
+  $(document).on('click', function closeMenu (e){
+    if(!$('nav').hasClass('opening') && $('nav').attr('class') != undefined && $('nav').has(e.target).length === 0){
+        $('nav').removeClass('open').addClass('close');
+    }
+  });
+
 });
